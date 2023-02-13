a5e0d3c2d2abfb3f50abbf521458ebe9a36c353c
nathalia-uf <112099626+nathalia-uf@users.noreply.github.com>
2023-02-12 22:34:55 -0500

    count

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ var words = [
   'ukulele',
   'mango'
 ]
+
+var previousWord = document.getElementById('previous-word')
+var incorrectLettersDisplay = document.getElementById('incorrect-letters')
+var remainingGuessDisplay = document.getElementById('remaining-guesses')
+var winDisplay = document.getElementById('wins')
+var lossDisplay = document.getElementById('losses')
+
+var wins = 0
+var losses = 0
+
+document.onkeyup = function(e) {
+  console.log(e.key)
+}
